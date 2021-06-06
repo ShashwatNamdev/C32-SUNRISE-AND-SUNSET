ffb2a104103b9b61d3f9485697944a81c81bffff
ShashwatNamdev <77872270+ShashwatNamdev@users.noreply.github.com>
2021-06-06 14:26:48 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -23,6 +23,9 @@ function setup(){
 
 function draw(){
     // add condition to check if any background image is there to add
+    if(backgroundImg){
+        background(backgroundImg);
+    }
 
 
     Engine.update(engine);
