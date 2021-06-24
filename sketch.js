0d3593cb66ff4cd5746018db1b7cc71f0bc23d80
ShashwatNamdev <77872270+ShashwatNamdev@users.noreply.github.com>
2021-06-24 13:10:45 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -43,29 +43,29 @@ async function getBackgroundImg(){
     var hour = datetime.slice(11,13);
     console.log(hour);
     
-    if(hour=00 && hour<02){
+    if(hour==00 && hour<=02){
         bg = "sunset11.png";
-    }else if(hour>02 && hour<=04){
+    }else if(hour>=02 && hour<=04){
         bg = "sunset12.png";
     }else if(hour>=04 && hour<=06){
         bg = "sunrise1.png";
-    }else if(hour>06 && hour<=08){
+    }else if(hour>=06 && hour<=08){
         bg = "sunrise2.png";
-    }else if(hour>08 && hour<=10){
+    }else if(hour>=08 && hour<=10){
         bg = "sunrise3.png";
-    }else if(hour>10 && hour<=12){
+    }else if(hour>=10 && hour<=12){
         bg = "sunrise4.png";
-    }else if(hour>12 && hour<=14){
+    }else if(hour>=12 && hour<=14){
         bg = "sunrise5.png";
-    }else if(hour>14 && hour<=16){
+    }else if(hour>=14 && hour<=16){
         bg = "sunrise6.png";
-    }else if(hour>16 && hour<=18){
+    }else if(hour>=16 && hour<=18){
         bg = "sunset7.png";
-    }else if(hour>18 && hour<=20){
+    }else if(hour>=18 && hour<=20){
         bg = "sunset8.png";
-    }else if(hour>20 && hour<=22){
+    }else if(hour>=20 && hour<=22){
         bg = "sunset9.png";
-    }else if(hour>22 && hour<=24){
+    }else if(hour>=22 && hour<=24){
         bg = "sunset10.png";
     }
     backgroundImg = loadImage(bg);
